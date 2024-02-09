4870e6592972674207a4dfa1d09a08ce07a3d0eb
João pedro Maciel pinheiro <shionlk98@gmail.com>
2024-02-09 20:36:44 -0300

    up-3

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,35 @@
-import { View, Text } from "react-native";
-import {
-    Inter_400Regular,
-    Inter_500Medium,
-    Inter_600SemiBold,
-    Inter_700Bold
-} from "@expo-google-fonts/inter";
+import { View ,FlatList} from "react-native";
+import { Header } from "@/components/header";
 
+import {CATEGORIES} from "@/utils/data/products"
+import { CategoryButton } from "@/components/category-button"
 
 
 
 export default function Home() {
+   let category = "Lanche do dia"
+   
+    function handleCategorySelect(selectedCategory: string){
+        console.log(selectedCategory)
+        category = selectedCategory
+    }
+
+
     return (
-        <View className=" flex-1 pt-8" >
-            <Text className="text-white text-2xl">Hello </Text>
+        <View className="flex-1 pt-8 " >
+            <Header title="Faça seu pedido" cartQuantityItems={10} />
+
+            <FlatList
+            data={CATEGORIES}
+            keyExtractor={(item) => item}
+            renderItem={({item}) => <CategoryButton title={item} 
+            isSelected={item === category} onPress={() => handleCategorySelect(item)} />}
+            horizontal
+            className="max-h-10 mt-5"    
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{gap:12 , paddingHorizontal:20}}
+            />
+
         </View>
     )
 }
