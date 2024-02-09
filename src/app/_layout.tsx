60edd11a618f5cc38834e1259b80901441131231
João pedro Maciel pinheiro <shionlk98@gmail.com>
2024-02-09 13:49:35 -0300

    up-2

---
+++ src/app/_layout.tsx
@@ -0,0 +1,15 @@
+import { SafeAreaView } from "react-native";
+import { Slot } from "expo-router";
+
+
+export default function Layout() {
+    return (
+        <SafeAreaView className="flex-1 bg-slate-900 " >
+
+            <Slot />
+
+        </SafeAreaView>
+
+    )
+
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,21 @@
+import { View, Text } from "react-native";
+import {
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold
+} from "@expo-google-fonts/inter";
+
+
+
+
+export default function Home() {
+    return (
+        <View className=" flex-1 pt-8" >
+            <Text className="text-white text-2xl">Hello </Text>
+        </View>
+    )
+}
+
+
+
